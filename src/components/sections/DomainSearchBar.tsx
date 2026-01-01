8acf88756ef9021b896e183b48f1d71cb11f9f00
Devprodipraj <devprodipraj@gmail.com>
2026-01-01 19:11:30 +0600

    change some design in domain find form

--- src/components/sections/DomainSearchBar.tsx
+++ src/components/sections/DomainSearchBar.tsx
@@ -26,8 +26,8 @@ export function DomainSearchBar() {
   };
 
   return (
-    <section className="bg-gradient-to-r from-light-dark via-primary/20 to-light-dark border-b border-border/30">
-      <div className="container mx-auto px-4 py-6 md:py-8">
+    <section className="bg-gradient-to-r from-light-dark via-primary/20 to-light-dark border-b border-border/30 ">
+      <div className="container mx-auto px-4 py-6 md:py-16">
         <div className="max-w-4xl mx-auto">
           {/* Header */}
           <div className="text-center mb-5">
